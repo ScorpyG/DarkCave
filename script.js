7bc70597c1095691ff239e600ce1f78b7f032ad9
Justin Hoang <69221471+ScorpyG@users.noreply.github.com>
2020-09-09 10:50:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleFunction() {
+    var x = document.getElementById("myTopNav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
