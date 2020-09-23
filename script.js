75cbbce19db00e867c85cbcad530d0d328487752
Justin Hoang <69221471+ScorpyG@users.noreply.github.com>
2020-09-22 17:44:17 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ function toggleFunction() {
         x.className = "topnav";
     }
 }
+
+const ScrollToTop_btn = document.getElementById("to-top-btn");
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 400) {
+        if (!ScrollToTop_btn.classList.contains("btnEnter")) {
+            ScrollToTop_btn.classList.remove("btnExit");
+            ScrollToTop_btn.classList.add("btnEnter");
+            ScrollToTop_btn.style.display = "block";
+        }
+        
+    } 
+    else {
+        if (ScrollToTop_btn.classList.contains("btnEnter")) {
+            ScrollToTop_btn.classList.remove("btnEnter");
+            ScrollToTop_btn.classList.add("btnExit");
+            setTimeout (function() {
+                ScrollToTop_btn.style.display = "none";
+            }, 250)
+        }
+    }
+}
+
+ScrollToTop_btn.addEventListener("click", toTopFunction);
+
+function toTopFunction() {
+    window.scrollTo (0, 0);
+}
